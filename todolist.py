9988c849d448e549fdfd789a87f50074f8f9f51d
sodaitanaka <sodai.tanaka@homula.jp>
2024-10-28 18:55:40 +0900

    Status manage system for TODO list

--- todolist.py
+++ todolist.py
@@ -1,32 +1,46 @@
 todo_list = []
 
+#新規TODOを追加する関数
 def add_todo(todo_item):
-  todo_list.append(todo_item)
+  todo_list.append({"task": todo_item, "status": "todo"})
   print(f"Added TODO: {todo_item}")
+  print(f"Current TODO List: {todo_list}")
 
+#追加済TODOを表示する関数
 def show_todos():
     if not todo_list:
       print("No TODOs found.")
     else:
       print("TODO List:")
       for i, todo in enumerate(todo_list, 1):
-        print(f"{i}. {todo}")
+        print(f"{i}. {todo['task']} - Status: {todo['status']}")
 
-def delete_todo(todo_item):
+#TODOのステータスを変更する関数
+def change_status(index):
   if 0 <= index < len(todo_list):
-    removed_todo = todo_list.pop(index)
-    print(f"Delete TODO: {removed_todo}")
+    todo_list[index]["status"] =  "done"
+    print(f"Status of TODO: '{todo_list[index]['task']}' changed to 'done'")
   else:
     print("Invalid index. Plese enter a valid TODO number.")
 
+#TODOを削除する関数
+def delete_todo(todo_item):
+  if 0 <= index < len(todo_list):
+    removed_todo = todo_list.pop(index)
+    print(f"Delete TODO: {removed_todo['task']}")
+  else:
+    print("Invalid index. Plese enter a valid TODO number.")
+
+#メインプログラム
 while True:
   print("/nSelect an option:")
   print("1. Add TODO")
   print("2. Show TODOs")
   print("3. Delete TODO")
-  print("4. Exit")
+  print("4. Change Status to Done")
+  print("5. Exit")
 
-  choice = input("Enter yuour choice (1/2/3/4):")
+  choice = input("Enter yuour choice (1/2/3/4/5):")
 
   if choice == '1':
     new_todo = input("Enter the new TODO:")
@@ -41,7 +55,14 @@ while True:
     except ValueError:
       print("Plese enter a valid number.")
   elif choice == '4':
+    show_todos()
+    try:
+      index = int(input("Enter the TODO number to mark as done:")) - 1
+      change_status(index)
+    except ValueError:
+      print("Plese enter a valid number.")
+  elif choice == '5':
     print("Exiting TODO List App.")
     break
   else:
-    print("invalid choice. Please enter 1, 2, 3, or 4.")
+    print("invalid choice. Please enter 1, 2, 3, 4, or 5.")
